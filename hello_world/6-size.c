bd9e9efa42585fec6ffbb3240a39b46fdba0a849
marwenrebah <imnotbunu@gmail.com>
2023-06-15 04:30:41 -0700

    size

---
+++ hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
+printf("Size of float: %zu bytes\n", sizeof(float));
+return (0);
+}
