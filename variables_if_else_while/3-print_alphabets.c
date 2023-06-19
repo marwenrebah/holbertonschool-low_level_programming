70494bcb121f0e07f726adb7d4b3bd1050942be9
marwenrebah <imnotbunu@gmail.com>
2023-06-19 05:10:57 -0700

    Alphabet in lowercase

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char x;
+for (x = 'A' ; x <= 'Z' ; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
+
