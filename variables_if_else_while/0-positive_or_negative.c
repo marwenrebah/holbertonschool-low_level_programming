7038ffeaf06391da0bfb032c1aeb3b116ebc9388
marwenrebah <imnotbunu@gmail.com>
2023-06-19 01:51:57 -0700

    positive or negative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - assign a random number to the  variable n
+ * compiled and run on.
+ *
+ * Return: Always 0.
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
