53e58f4ca45a6a4080b15a86366f852c39073c18
marwenrebah <imnotbunu@gmail.com>
2023-06-21 02:24:13 -0700

    print reverse alpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 122; x > = 97 ; x--)
+putchar(x);
+putchar('\n');
+return (0);
+}
