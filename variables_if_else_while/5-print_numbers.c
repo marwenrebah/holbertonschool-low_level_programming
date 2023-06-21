0b505c512d08676a32246cf878386ef83205b5ec
marwenrebah <imnotbunu@gmail.com>
2023-06-21 01:34:32 -0700

    print 0 to 10

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char d;
+d = '0';
+while (d <= '9')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
