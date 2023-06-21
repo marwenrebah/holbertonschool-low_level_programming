54b358157b5aa74d5e924ef2e591cc19800faed0
marwenrebah <imnotbunu@gmail.com>
2023-06-21 03:03:09 -0700

    print com

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+x = 48;
+while (x <= 57)
+{
+	putchar(',');
+	putchar(x);
+	x++;
+}
+putchar('\n');
+return (0);
+}
