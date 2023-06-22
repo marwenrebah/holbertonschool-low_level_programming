cd770adf4ff1d0751b6cbeccd0cf298cf6e4af52
marwenrebah <imnotbunu@gmail.com>
2023-06-22 05:08:29 -0700

    prints last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit -Prints the last digit of a number
+ * @x: numbmer
+ * Return: 0
+ */
+int print_last_digit(long x)
+{
+if (x < 0)
+	x *= -1;
+_putchar(0 + (x % 10));
+return (x % 10);
+}
