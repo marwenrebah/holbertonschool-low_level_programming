e30de90e5e874210e3feb70cabcbe120d5693662
marwenrebah <imnotbunu@gmail.com>
2023-06-22 05:42:17 -0700

    every minute of the day

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -4,26 +4,26 @@
  */
 void jack_bauer(void)
 {
-int a, b, c, d;
+int a, b, c, d, e = 9;
 for (a = 0; a <= 2; a++)
 {
-for (b = 0; b <= 9; b++)
+for (b = 0; b <= e; b++)
 {
-if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-{
+if (a == 2)
+	e = 3;
 for (c = 0; c <= 5; c++)
 {
-for (d = 0; d <= 0; d++)
+for (d = 0; d <= 9; d++)
 {
-_putchar(a + '0');
-_putchar(b + '0');
-_putchar(50);
-_putchar(c + '0');
-_putchar(d + '0');
+_putchar(a + 48);
+_putchar(b + 48);
+_putchar(':');
+_putchar(c + 48);
+_putchar(d + 48);
 _putchar('\n');
 }
 }
 }
 }
 }
-}
+
