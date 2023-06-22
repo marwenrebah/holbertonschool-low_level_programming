55e29ce6c84331696c15239ce5de2822d2443d43
marwenrebah <imnotbunu@gmail.com>
2023-06-22 01:01:34 -0700

    lowercase

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char x = 'a';
-	while (x <= 'z')
-	{
-		_putchar(x);
-		x++;
-	}
-	_putchar('\n');
-    return (0);
+char x = 'a';
+while (x <= 'z')
+{
+_putchar(x);
+x++;
+}
+_putchar('\n');
+return (0);
 }
