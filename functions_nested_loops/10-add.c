5af4a0d51362a29584ac29573ebd8ec381b49596
marwenrebah <imnotbunu@gmail.com>
2023-06-22 10:24:01 -0700

    two integers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add -two integers
+ * @a: the first given number
+ * @b: the second given number
+ * Return: n
+ */
+int add(int a, int b)
+{
+return (a + b);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,4 +11,5 @@ int _abs(int);
 int print_last_digit(long x);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
 #endif
