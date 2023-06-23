a928f6d5c374dbdf58bf723e00993752832c2a3f
marwenrebah <imnotbunu@gmail.com>
2023-06-23 14:07:44 -0700

    print triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle -print a triangle
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+if (size > 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = size - a; b > 0; b--)
+_putchar(' ');
+for (b = 0; b < a; b++)
+_putchar('#');
+if (a == size)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
