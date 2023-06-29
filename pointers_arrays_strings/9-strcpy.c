793071706a05b6b53723186bcb58c177e280e148
marwenrebah <imnotbunu@gmail.com>
2023-06-29 06:39:53 -0700

    copies the string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,12 +8,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-int x;
-x = 0;
-while (src[x])
+int x = 0;
+while (*(src + x) != '\0')
 {
-	dest[x] = src[x];
+*(dest + x) = *(src + x);
 	x++;
 }
+*(dest + x) = '\0';
 return (dest);
 }
