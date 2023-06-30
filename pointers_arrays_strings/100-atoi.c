661854fc7b5265b157313c47fec0f6bab513d4c8
marwenrebah <imnotbunu@gmail.com>
2023-06-29 17:41:15 -0700

    Convert a string to integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,22 +1,62 @@
-#include "main.h"
+#include <limits.h>
 /**
- * _atoi - function
- *@s: char
- * Return: int
+ * calcul_neg - calculate (-) in string
+ * @ch: the given string
+ * Return: neg
+ */
+int calcul_neg(char *ch)
+{
+int i = 0, neg = 0;
+while (ch[i] != '\0' && (ch[i] > '9' || ch[i] < '0'))
+{
+if (ch[i] == '-')
+neg++;
+i++;
+}
+return (neg);
+}
+/**
+ *_strlen - calculates the length of a string
+ *@ch: the string
+ *Return: len
+ */
+int _strlen(char *ch)
+{
+int i = 0, len = 0;
+while (ch[i] != '\0')
+{
+len++;
+i++;
+}
+return (len);
+}
+/**
+ *_atoi - converts a string into an integer
+ *@s: the string
+ *
+ *Return: n
  */
 int _atoi(char *s)
 {
-int number = 0, x, sign = 1;
-for (x = 0; s[x] != '\0'; x++)
+int len = _strlen(s), i = len - 1, n = 0, j = 1, neg, R;
+neg = calcul_neg(s);
+if (neg % 2)
+j = -1;
+R = j;
+while (i >= 0)
 {
-if (s[x] == '-')
+if (s[i] <= '9' && s[i] >= '0')
 {
-sign *= -1;
-}
-if (s[x] >= '0' && s[x] <= '9')
+if (n != 0 && (s[i + 1] < '0' || s[i + 1] > '9'))
 {
-number = number * 10 + s[x] - '0';
+n = 0;
+j = R;
 }
+n += (s[i] - '0') * j;
+if (j != 1000000000 && j != -1000000000)
+j *= 10;
 }
-return (number *sign);
+i--;
+}
+return (n);
 }
