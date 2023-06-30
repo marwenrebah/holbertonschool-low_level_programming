abc64119020c0787aa25d7a31662c67da5fcd3a0
marwenrebah <imnotbunu@gmail.com>
2023-06-30 15:37:52 -0700

    copy n characters

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,45 +1,56 @@
 #include <stdlib.h>
+
 /**
- * _strlen - calculates length of a string
- *@ch: given string
- *Return: len
+ * _strlen - calculates length of
+ * a given string
  *
+ * @ch: given string
+ *
+ * Return: len
  */
+
 int _strlen(char *ch)
 {
-int i = 0, len = 0;
-while (*(ch + i) != '\0')
-{
-len++;
-i++;
-}
-return (len);
+	int i = 0, len = 0;
+
+	while (*(ch + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
 }
 /**
- * _strncpy - copies a string
- *@dest: first one
- *@src: second one
- *@n: number of characters
+ * _strncpy - copy n characters
+ *
+ * @dest: destination
+ *
+ * @src: copy source
+ *
+ * @n: number of characters
+ *
  * Return: copied to
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
-int i = 0, j = 0;
-while (j < n)
-{
-*(dest + i) = *(src + j);
-if (*(src + j) == '\0')
-j--;
-j++;
-i++;
-}
-if (*(src + i) == '\0')
-{
-while (i < _strlen(dest))
-{
-*(dest + i) = '\0';
-i++;
-}
-}
-return (dest);
+	int i = 0, j = 0;
+
+	while (i < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			j--;
+		j++;
+		i++;
+	}
+	if (*(src + i) == '\0')
+	{
+		while (i < _strlen(dest))
+		{
+			*(dest + i) = '\0';
+			i++;
+		}
+	}
+	return (dest);
 }
