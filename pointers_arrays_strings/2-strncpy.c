8292b19f5de7e6022712dfeacbbc971a75439199
marwenrebah <imnotbunu@gmail.com>
2023-06-30 09:16:30 -0700

    copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,20 @@
 #include "main.h"
 #include <stdio.h>
+#include <stddef.h>
 /**
  * _strncpy - copies a string
  *@dest: first one
  *@src: second one
  *@n: number of characters
- * Return: copied to
+ * Return: char
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-char *index;
-index = dest;
+char *ptr = dest;
 if (dest == NULL)
 {
 return (NULL);
 }
-char *ptr = dest;
 while (*src && n--)
 {
 *dest = *src;
