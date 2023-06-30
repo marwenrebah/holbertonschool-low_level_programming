e418658f90eadc2ab6f3f6e21eb6db1d1d90ca87
marwenrebah <imnotbunu@gmail.com>
2023-06-30 09:58:04 -0700

    copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,6 +2,22 @@
 #include <stdio.h>
 #include <stddef.h>
 /**
+ * _strlen - calculates length of a string
+ *@ch: given string
+ *Return: len
+ *
+ */
+int _strlen(char *ch)
+{
+int i = 0, len = 0;
+while (*(ch + i) != '\0')
+{
+len++;
+i++;
+}
+return (len);
+}
+/**
  * _strncpy - copies a string
  *@dest: first one
  *@src: second one
@@ -22,5 +38,5 @@ while (*src && n--)
 dest++;
 src++;
 }
-return(ptr);
+return (ptr);
 }
