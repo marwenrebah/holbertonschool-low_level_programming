45adb0763fdab4aa22fc8d2881fc60db787b1f78
marwenrebah <imnotbunu@gmail.com>
2023-07-03 03:52:00 -0700

    uses recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - uses recursion
+ *@s: string
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+if (*s)
+{
+	_putchar(*s);
+	_puts_recursion(++s);
+}
+else
+{
+	_putchar('\n');
+}
