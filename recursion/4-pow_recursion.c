49f00ffcacdac67c110c717164f538b94b1ba0e8
marwenrebah <imnotbunu@gmail.com>
2023-07-04 01:40:21 -0700

    value of x raised to the power of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *@x: n
+ *@y: power
+ * Return: the value
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+else if (y == 0)
+return (1);
+x *= _pow_recursion(x, y - 1);
+return (x);
+}
