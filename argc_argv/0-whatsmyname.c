cf7330e3f9e165018db45c3aaac817f896a90799
marwenrebah <imnotbunu@gmail.com>
2023-07-05 03:02:21 -0700

    argc argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that print its name followed by new line
+ * @argc : argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
