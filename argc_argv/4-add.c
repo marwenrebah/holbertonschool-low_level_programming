888681eb30c723a83dfecf5dbdbc6542dcbbd0e6
marwenrebah <imnotbunu@gmail.com>
2023-07-05 03:48:58 -0700

    adds positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * main - adds positive numbers
+ * @argc: argc
+ * @argv: argv
+ * Return: 0 if no number is passed, 1 if number contains symbols
+ */
+int main(int argc, char **argv)
+{
+int n,d, s = 0;
+for (n = 1; n < argc; n++)
+{
+for (d = 0; argv[n][d]; d++)
+{
+if (argv[n][d] < '0' || argv[n][d] > '9')
+return printf("Error\n"), 1;
+}
+s += atoi(argv[n]);
+}
+printf("%d\n", s);
+return (0);
+}
