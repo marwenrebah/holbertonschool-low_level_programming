6439ad9365c110490eac3811af1387d2f4016f6b
marwenrebah <imnotbunu@gmail.com>
2023-07-05 03:26:03 -0700

    argc prints all arguments

---
+++ argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  prints all arguments it receives per line
+ * @argc: argc
+ * @argv: argv
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
