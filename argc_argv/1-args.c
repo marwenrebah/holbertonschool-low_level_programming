61bae779ff015c167e9443702f6902b53e4d8466
marwenrebah <imnotbunu@gmail.com>
2023-07-05 03:20:14 -0700

    argc -1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -3,11 +3,11 @@
  * main - prints the number of arguments
  * @argc: argc
  * @argv: argv
- * Return: 0
+ * Return: void
  */
 int main(int argc, char **argv)
 {
 (void)argv;
-printf("%d\n", argc);
+printf("%d\n", argc - 1);
 return (0);
 }
