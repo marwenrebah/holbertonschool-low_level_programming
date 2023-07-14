44a8e2815668f902c3a01ae16379fc1aee1cff09
marwenrebah <imnotbunu@gmail.com>
2023-07-14 06:47:37 -0700

    frees a 2 dimensional grid

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -7,8 +7,8 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
-	for (i = 0; i < height; i++)
-		free(grid[i]);
-			free(grid);
+int i;
+for (i = 0; i < height; i++)
+free(grid[i]);
+free(grid);
 }
