9699381f876eeacf2d675fb4126187c86c034df2
marwenrebah <imnotbunu@gmail.com>
2023-07-14 07:29:59 -0700

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,16 +1,46 @@
-#include"main.h"
+#include "main.h"
 #include<stdio.h>
 #include<stdlib.h>
-/**
- * malloc_checked - a function that allocates memory using malloc
- * @b: int
- * Return: Pointer to the allocated memory or NULL
- */
-void *malloc_checked(unsigned int b)
-{
-void *loc = malloc(b);
-if (!loc)
-exit(98);
-return (loc);
-}
 
+/**
+* string_nconcat - a function that concatenates two strings.
+*
+* @s1: first char
+* @s2: secound char
+* @n: unsigned int
+* Return: If the function fails, it should return NULL
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int x, y, z;
+char *s;
+if (s1 == NULL)
+{
+x = 0;
+}
+else
+{
+for (x = 0; s1[x]; ++x)
+;
+}
+if (s2 == NULL)
+{
+y = 0;
+}
+else
+{
+for (y = 0; s2[y]; ++y)
+;
+}
+if (y > n)
+y = n;
+s = malloc(sizeof(char) * (x + y + 1));
+if (s == NULL)
+return (NULL);
+for (z = 0; z < x; z++)
+s[z] = s1[z];
+for (z = 0; z < y; z++)
+s[z + x] = s2[z];
+s[x + y] = '\0';
+return (s);
+}
