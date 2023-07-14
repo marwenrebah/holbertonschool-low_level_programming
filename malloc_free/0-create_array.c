738417adf692685a0b9df35f1773583218f49169
marwenrebah <imnotbunu@gmail.com>
2023-07-14 06:07:53 -0700

    create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -7,15 +7,15 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i;
-	char *ch;
-	if (!size)
-		return (NULL);
-	ch = (char *) malloc(sizeof(char) * size);
-	if ( ch != NULL)
-	{
-		for (i = 0; i < size; i++)
-			*(ch + i) = c;
-	}
-	return (ch);
+unsigned int i;
+char *ch;
+if (!size)
+return (NULL);
+ch = (char *) malloc(sizeof(char) * size);
+if (ch != NULL)
+{
+for (i = 0; i < size; i++)
+*(ch + i) = c;
+}
+return (ch);
 }
