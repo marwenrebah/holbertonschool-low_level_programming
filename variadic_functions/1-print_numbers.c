edba4e499b0efeaa7e117a49307457a4544b4ea7
marwenrebah <6863@holbertonstudents.com>
2023-07-21 04:26:56 -1200

    print the given numbers seperated by a given seperator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_numbers - print the given numbers seperated by a given sepertaor
+ * @separator: the given string to seperate numbers
+ * @n: count of numbers
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int x;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+x = va_arg(args, int);
+(separator != NULL && i != n - 1) ?
+printf("%d%s", x, separator) : printf("%d", x);
+}
+putchar("\n")
+}
