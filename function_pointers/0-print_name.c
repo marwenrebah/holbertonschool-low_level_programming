63f98473714bc584f9cf33bc0c57c6f501dafe3d
marwenrebah <6863@holbertonstudents.com>
2023-07-20 21:01:04 -1200

    Print name of the person

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
 * print_name - Print a name
 * @name: name of the person
 * @f: the function to realise that
-**/
+*/
+
 void print_name(char *name, void (*f)(char *))
 {
 if (f != NULL)
