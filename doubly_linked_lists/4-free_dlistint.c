19a00306aace18dc53049c79a895f7f2466c9ca0
marwenrebah <6863@holbertonstudents.com>
2023-08-03 22:17:42 -1200

    frees a dlistint_t

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_dlistint - Adds a new node at the end of a dlistint_t list.
+ * @head: A pointer to the head of the dlistint_t list.
+ */
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *s;
+while (head)
+{
+s = head->next;
+free(head);
+head = s;
+}
+}
